f7bec30e68c88e510ab45a0f4b118a71f7eec49c
Alexander Jipa <alexander.jipa@gmail.com>
2024-01-07 01:12:09 -0500

    feat: helix openai key binding

---
+++ .config/helix/openai.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import json
+import os
+import sys
+from urllib import request
+
+
+ask = sys.stdin.read()  # Will contain the multiline question
+
+model = "gpt-3.5-turbo"
+data = {
+    "model": model,
+    "messages": [
+        {
+            "role": "system",
+            "content": "You are an expert in programming and should provide only code.",
+        },
+        {
+            "role": "user",
+            "content": ask,
+        },
+    ],
+}
+
+OPENAI_API_KEY = os.environ["CHATGPT_API_KEY"]
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {OPENAI_API_KEY}",
+}
+url = "https://api.openai.com/v1/chat/completions"
+req = request.Request(url, method="POST")
+req.add_header("Content-Type", "application/json")
+req.add_header("Authorization", f"Bearer {OPENAI_API_KEY}")
+data = json.dumps(data)
+data = data.encode()
+
+r = request.urlopen(req, data=data)
+if r.getcode() != 200:
+    print(r.getcode(), r.read())
+else:
+    response = json.loads(r.read())
+    answer = response["choices"][0]["message"]["content"]
+    print(ask, answer)

60f3d9568aa2707daed79781d968c2f1f6df5b2f
Alexander Jipa <alexander.jipa@gmail.com>
2024-03-08 12:57:31 -0500

    feat: add WezTerm config

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'nord'
+config.font = wezterm.font 'Fira Code Retina'
+config.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendString '\x1bb',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendString '\x1bf',
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
